7372ad818e424e324d0e505e9e74677bc8769423
SeerLite <seerlite@disroot.org>
2024-02-26 18:15:18 -0300

    Change it from a 12-hour to a 24-hour interval

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -44,9 +44,12 @@ export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
   const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
   const now = Date.now()
-  const msInDay = 43200000 //modified to play every 12 hours (12*60*60*1000)
-  const index = Math.floor((now - epochMs) / msInDay)
-  const nextday = (index + 1) * msInDay + epochMs
+  const msInDay = 86400000 // every 24 hours (24*60*60*1000)
+  const today = Math.floor((now - epochMs) / msInDay)
+  // Add 787 which is (index_when_we_played_12 - index_if_we_always_played_24) of the afternoon I changed it from a 12-hour to a 24-hour interval
+  // Made the change seamless
+  const index = 787 + today
+  const nextday = (today + 1) * msInDay + epochMs
 
   return {
     solution: WORDS[index % WORDS.length].toUpperCase(),
